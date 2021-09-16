e4cc046534928b44293c731b4a7063c1901740a6
Cracker133 <vamsi7622@gmail.com>
2021-09-16 09:50:38 +0530

    Second day

--- main.py
+++ main.py
@@ -1,15 +1,21 @@
-from flask import Flask
+from flask import Flask,render_template,request
 
 #WSGI
 app = Flask(__name__)
 
-@app.route('/',methods=['GET'])
+@app.route('/')
 def welcome():
-    return "Welcome to Praxis"
+    return render_template("index.html")
 
 @app.route('/name/<your_name>')
 def names(your_name):
     return f"Welcome to praxis {your_name}"
+
+@app.route('/checking_req',methods=['POST','GET'])
+def get_req_parameters():
+    name= request.args.get("student_name")
+    roll_no =request.args.get("roll_no")
+    return f"Student name is {name} and roll number is {roll_no}",206
 
 if __name__ == "__main__":
     app.run(debug =True)
