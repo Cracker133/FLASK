2f23597350145c3a56612a2b96899f0c5edfe349
Cracker133 <vamsi7622@gmail.com>
2021-09-21 09:10:25 +0530

    Third

--- main2.py
+++ main2.py
@@ -1,38 +1,47 @@
-from flask import Flask,render_template,request
+# Importing the required package
+from flask import Flask, render_template, request
+#import flasgger
 from flasgger import Swagger
 
-#WSGI
+# Defining the name (will fetch from the actual file name)
 app = Flask(__name__)
 Swagger(app)
 
-@app.route('/')
+# Defingin the path or url attahced after the local ip
+@app.route('/', methods=['GET'])
+# Function that would be executed when the above URL is hit.
 def welcome():
-    return "Learning Swagger building"
-@app.route('/name/<your_name>')
-def names(your_name):
-    return f"Welcome to praxis {your_name}"
+    return render_template("index.html")
 
+# Defining second url
+@app.route('/name/<your_name>')
+# Function that should be executed when this URL is hit
+def names(your_name):
+    return f"Welcome to Praxis {your_name}"
+# Defining using Swagger
 @app.route('/checking_req',methods=['POST','GET'])
 def get_req_parameters():
-
     """ Practicing Swagger
     ---
     parameters:
-      - name: Student_name
-        in: query
-        type: string
-        required:true
-      - name: roll_no
-        in: query
-        type: number
-        required:true
+     - name: Student_name
+       in: query
+       type: string
+       required: true
+     - name: roll_no
+       in: query
+       type: number
+       required: true
     responses:
-        200:
+       200:
             description: Result is
+
     """
     name = request.args.get("Student_name")
-    num = request.args.get("roll_no")
-    return f"Student name is {name} and roll number is {num} in praxis" 
+    roll_no = request.args.get("roll_no")
+    return f"Student name is {name} and roll no is {roll_no} in Praxis"
+
 
 if __name__ == "__main__":
-    app.run(debug =True)
+    # Use debug only when required. With debug no need to stop the server every time.
+    app.run(debug=True)
